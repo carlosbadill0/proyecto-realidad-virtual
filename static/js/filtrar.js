2297d523406d52c3b4bdf9cbce9912bf89c44c0e
luissann <luis.sanhueza2001@alumnos.ubiobio.cl>
2024-10-13 21:36:43 -0300

    cambios visuales

--- static/js/filtrar.js
+++ static/js/filtrar.js
@@ -1,29 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
     const searchInput = document.getElementById('searchInput');
     const table = document.getElementById('userTable');
-    const rows = table.getElementsByTagName('tr');
+    const noResultsMessage = document.getElementById('noResultsMessage');
 
-    searchInput.addEventListener('input', function() {
-        const filter = searchInput.value.toLowerCase();
+    if (searchInput && table && noResultsMessage) {
+        const rows = table.getElementsByTagName('tr');
 
-        for (let i = 1; i < rows.length; i++) { // Start from 1 to skip table header
-            let cells = rows[i].getElementsByTagName('td');
-            let match = false;
+        searchInput.addEventListener('input', function() {
+            const filter = searchInput.value.toLowerCase();
+            let hasResults = false;
 
-            for (let j = 0; j < cells.length; j++) {
-                if (cells[j]) {
-                    if (cells[j].innerText.toLowerCase().indexOf(filter) > -1) {
-                        match = true;
-                        break;
+            for (let i = 1; i < rows.length; i++) { // Start from 1 to skip table header
+                let cells = rows[i].getElementsByTagName('td');
+                let match = false;
+
+                for (let j = 0; j < cells.length; j++) {
+                    if (cells[j]) {
+                        if (cells[j].innerText.toLowerCase().indexOf(filter) > -1) {
+                            match = true;
+                            break;
+                        }
                     }
+                }
+
+                if (match) {
+                    rows[i].style.display = '';
+                    hasResults = true;
+                } else {
+                    rows[i].style.display = 'none';
                 }
             }
 
-            if (match) {
-                rows[i].style.display = '';
+            if (hasResults) {
+                noResultsMessage.style.display = 'none';
             } else {
-                rows[i].style.display = 'none';
+                noResultsMessage.style.display = 'block';
             }
-        }
-    });
+        });
+    }
 });
